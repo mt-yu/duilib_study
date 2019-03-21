66ad80b0730c5488b687f06d4f90498b5ef901d4
mt-yu <30967271+mt-yu@users.noreply.github.com>
2019-03-21 16:27:24 +0800

    第一次提交

---
+++ Duilib_test/CDuiFrameWnd.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <UIlib.h>
+using namespace DuiLib;
+
+
+
+class CDuiFrameWnd : public CWindowWnd, public INotifyUI
+{
+public:
+	virtual LPCTSTR GetWindowClassName() const { return _T("DUIMainFrame"); }
+	virtual void Notify(TNotifyUI& msg) {}
+
+	virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
+	{
+		LRESULT lRes = 0;
+
+		if (uMsg == WM_CREATE)
+		{
+			CControlUI *pWnd = new CButtonUI;
+			pWnd->SetText(_T("Hello World"));   // 设置文字
+			pWnd->SetBkColor(0xFF00FFFF);       // 设置背景色
+
+			m_PaintManager.Init(m_hWnd);
+			m_PaintManager.AttachDialog(pWnd);
+			return lRes;
+		}
+
+		if (m_PaintManager.MessageHandler(uMsg, wParam, lParam, lRes))
+		{
+			return lRes;
+		}
+
+		return __super::HandleMessage(uMsg, wParam, lParam);
+	}
+
+protected:
+	CPaintManagerUI m_PaintManager;
+};
---
+++ Duilib_test/Duilib_test.cpp
@@ -0,0 +1,23 @@
+﻿// Duilib_test.cpp : 定义应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "Duilib_test.h"
+#include "CDuiFrameWnd.h"
+
+
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+	CPaintManagerUI::SetInstance(hInstance);
+
+	CDuiFrameWnd duiFrame;
+	duiFrame.Create(NULL, _T("DUIWnd"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+	duiFrame.ShowModal();
+
+    return 0;
+}
+
